aa5d0982845cae8f85a0e64da5e05eb773fc8fcf
emschenn <emilynov14@gmail.com>
2017-03-13 13:45:34 +0800

    add A.cpp into repository

--- A.cpp
+++ A.cpp
@@ -6,13 +6,18 @@ int main()
 	cout << "input: ";
 	cin >> x;
 	cout << x << " ";
-	while(x!=1)
+	if(x==0)
+		cout << 0;
+	else
 	{
-		if(x%2==1)
-			x = 3*x+1;
-		else
-			x = x/2;
-		cout << x << " " ;
+		while(x!=1)
+		{
+			if(x%2==1)
+				x = 3*x+1;
+			else
+				x = x/2;
+			cout << x << " " ;
+		}
 	}
 	return 0;	
 }
